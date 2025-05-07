8ef567858058738be44dea9d9cbac8aa66d1c6cf
LoganDawes <logantdawes@gmail.com>
2025-05-07 10:18:50 -0500

    Added review adding & deleting

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -41,8 +41,32 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.session.authorization.username;
+
+    if (books[isbn]) {
+        if (!books[isbn].reviews) {
+            books[isbn].reviews = {};
+        }
+        books[isbn].reviews[username] = review;
+        return res.status(200).send("Review added successfully");
+    } else {
+        return res.status(404).send("Book not found");
+    }
+});
+
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+
+    if (books[isbn] && books[isbn].reviews && books[isbn].reviews[username]) {
+        delete books[isbn].reviews[username];
+        return res.status(200).send("Review deleted successfully");
+    } else {
+        return res.status(404).send("Review not found");
+    }
 });
 
 module.exports.authenticated = regd_users;
